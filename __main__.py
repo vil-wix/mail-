b05b7449d153f4a5ce970ad28856dbfe38e5dc82
vil-wix <vil.wix.2005@gmail.com>
2024-11-04 01:15:57 +0600

    Add files via upload

---
+++ __main__.py
@@ -0,0 +1,20 @@
+#1st program
+print('(9**0.5)*5')
+print((9**0.5)*5)
+
+#2nd program
+print('9.99 >9,98 and 1000 != 1000.1')
+print(9.99 >9,98 and 1000 != 1000.1)
+
+#3rd program'
+print('2 * 2 + 2 ')
+print('2 * (2 + 2) ')
+print((2 * 2 + 2 )==(2 * (2 + 2)) )
+
+#4th program
+a=str('123.456')
+a=(float(a))
+a=a*10
+a=int(a)
+a=a%10
+print(a)
